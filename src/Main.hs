00b034407533810960027f66fed1920420ffb9cc
Abbath <pitongogi@gmail.com>
2015-11-25 17:42:28 +0100

    Parentheses and fixes. Prompt

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,16 @@
 module Main where
 
 import Data.Char
+import System.IO
+import qualified Text.Read as R
+import Data.Maybe
 
 data Expr = Number Double
             | Sum Char Expr Expr
             | Mul Char Expr Expr
             | UMinus Expr
+            | Par Expr
+            deriving Eq
 
 instance Show Expr where
     show = showExpr 0
@@ -13,8 +18,13 @@ instance Show Expr where
 showExpr n (Sum op e1 e2) = replicate n ' ' ++ "Sum " ++ [op] ++ "\n" ++ showExpr (n+1) e1 ++ "\n" ++ showExpr (n+1) e2
 showExpr n (Mul op e1 e2) = replicate n ' ' ++ "Mul " ++ [op] ++ "\n" ++ showExpr (n+1) e1 ++ "\n" ++ showExpr (n+1) e2
 showExpr n (Number x ) =  replicate n ' ' ++ "Number " ++ show x
+showExpr n (Par e) = replicate n ' ' ++ "Par \n" ++ showExpr (n+1) e
 showExpr n (UMinus e) =  replicate n ' ' ++ "UMinus \n" ++ showExpr (n+1) e
 
+simplify e = simplify' e e
+    where simplify' e o = if simplifyExpr e == o then o else simplify' (simplifyExpr e) e
+
+simplifyExpr (Par e) = Par (simplifyExpr e)
 simplifyExpr (Mul '/' e1 (Mul '/' e2 e3)) = Mul '/' (simplifyExpr e1) (Mul '*' (simplifyExpr e2) (simplifyExpr e3))
 simplifyExpr (Sum '+' (Number 0.0) n) = simplifyExpr n
 simplifyExpr (Sum _ n (Number 0.0)) = simplifyExpr n
@@ -34,6 +44,7 @@ eval (Mul '/' x y) = let n = eval y
 eval (Mul '%' x y) = let n = eval y
                      in if n == 0 then error "Div by zero" else fromIntegral $ mod (floor . eval $ x) (floor n)
 eval (UMinus x) = -(eval x)
+eval (Par e) = eval e
 
 parseExpr s = let b = head s == '-'
                   ss = if b then tail s else s
@@ -52,32 +63,40 @@ parseTerm s = let (s1, s2) = breakPar (`elem` "*/%") s
                   e2 = if null s2 then Number 1 else parseExpr . tail $ s2
               in  Mul op e1 e2
 
-parseNumber s |"" == (snd . head . (reads :: String -> [(Double, String)]) $ s) =
+checkNumber s = let x = (reads :: String -> [(Double, String)]) s
+                in not (null x) && ("" == (snd . head $ x))
+
+parseNumber s |checkNumber s =
                     let x = fst . head . (reads :: String -> [(Double, String)]) $ s
                     in Number x
               |head s == '(' =
-                    let ss = tail . fst $ takePar 1 s []
-                    in parseExpr ss
+                    let ss = init . fst . takePar . tail $ s
+                    in Par (parseExpr ss)
               |otherwise = parseExpr s
 
-takePar n [] acc = if n == 0 then (reverse acc,[]) else error "Wrong!"
-takePar n (x:xs) acc = if n == 0
+takePar s = takePar' 1 s []
+
+takePar' n [] acc = if n == 0 then (reverse acc,[]) else error "Wrong!"
+takePar' n (x:xs) acc = if n == 0
                        then (reverse acc, x:xs)
                        else case x of
-                                    ')' -> takePar (n-1) xs (x:acc)
-                                    '(' -> takePar (n+1) xs (x:acc)
-                                    _ -> takePar n xs (x:acc)
+                                    ')' -> takePar' (n-1) xs (x:acc)
+                                    '(' -> takePar' (n+1) xs (x:acc)
+                                    _ -> takePar' n xs (x:acc)
 
 breakPar _ xs@[]           =  (xs, xs)
 breakPar p xs@(x:xs')
-           | x == '('   = let (a, b) = takePar 1 xs' []
+           | x == '('   = let (a, b) = takePar xs'
                               (y, z) = breakPar p b
                           in ([x] ++ a ++ y, z)
            | p x        =  ([],xs)
            | otherwise  =  let (ys,zs) = breakPar p xs' in (x:ys,zs)
 
 main = do
-    let x = "1/2/3"
-    let y =  simplifyExpr $ simplifyExpr $ parseExpr x
+    putStr "> "
+    hFlush stdout
+    x <- getLine
+    let y =  simplify $ parseExpr $ filter (not . isSpace) x
     print y
     print $ eval y
+    main
