a36fad69b70f8edac3222453c4ab0dfa08189552
Abbath <pitongogi@gmail.com>
2015-11-25 15:48:18 +0100

    Initial

---
+++ src/Main.hs
@@ -0,0 +1,83 @@
+module Main where
+
+import Data.Char
+
+data Expr = Number Double
+            | Sum Char Expr Expr
+            | Mul Char Expr Expr
+            | UMinus Expr
+
+instance Show Expr where
+    show = showExpr 0
+
+showExpr n (Sum op e1 e2) = replicate n ' ' ++ "Sum " ++ [op] ++ "\n" ++ showExpr (n+1) e1 ++ "\n" ++ showExpr (n+1) e2
+showExpr n (Mul op e1 e2) = replicate n ' ' ++ "Mul " ++ [op] ++ "\n" ++ showExpr (n+1) e1 ++ "\n" ++ showExpr (n+1) e2
+showExpr n (Number x ) =  replicate n ' ' ++ "Number " ++ show x
+showExpr n (UMinus e) =  replicate n ' ' ++ "UMinus \n" ++ showExpr (n+1) e
+
+simplifyExpr (Mul '/' e1 (Mul '/' e2 e3)) = Mul '/' (simplifyExpr e1) (Mul '*' (simplifyExpr e2) (simplifyExpr e3))
+simplifyExpr (Sum '+' (Number 0.0) n) = simplifyExpr n
+simplifyExpr (Sum _ n (Number 0.0)) = simplifyExpr n
+simplifyExpr (Mul '*' (Number 1.0) n) = simplifyExpr n
+simplifyExpr (Mul _ n (Number 1.0)) = simplifyExpr n
+simplifyExpr (Sum op e1 e2) = Sum op (simplifyExpr e1) (simplifyExpr e2)
+simplifyExpr (Mul op e1 e2) = Mul op (simplifyExpr e1) (simplifyExpr e2)
+simplifyExpr x = x
+
+eval :: Expr -> Double
+eval (Number x) = x
+eval (Sum '+' x y) = eval x + eval y
+eval (Sum '-' x y) = eval x - eval y
+eval (Mul '*' x y) = eval x * eval y
+eval (Mul '/' x y) = let n = eval y
+                     in if n == 0 then error "Div by zero" else eval x / n
+eval (Mul '%' x y) = let n = eval y
+                     in if n == 0 then error "Div by zero" else fromIntegral $ mod (floor . eval $ x) (floor n)
+eval (UMinus x) = -(eval x)
+
+parseExpr s = let b = head s == '-'
+                  ss = if b then tail s else s
+                  (s1, s2) = breakPar (`elem` "+-") ss
+                  e1 = parseTerm s1
+                  op = if null s2 then '+' else head s2
+                  e2 = if null s2 then Number 0 else case op of
+                     '+' -> parseExpr . tail $ s2
+                     '-' -> parseExpr . ('-':) . tail $ s2
+              in Sum '+' (if b then UMinus e1 else e1) e2
+
+parseTerm ('-':xs) = UMinus (parseTerm xs)
+parseTerm s = let (s1, s2) = breakPar (`elem` "*/%") s
+                  e1 = parseNumber s1
+                  op = if null s2 then '*' else head s2
+                  e2 = if null s2 then Number 1 else parseExpr . tail $ s2
+              in  Mul op e1 e2
+
+parseNumber s |"" == (snd . head . (reads :: String -> [(Double, String)]) $ s) =
+                    let x = fst . head . (reads :: String -> [(Double, String)]) $ s
+                    in Number x
+              |head s == '(' =
+                    let ss = tail . fst $ takePar 1 s []
+                    in parseExpr ss
+              |otherwise = parseExpr s
+
+takePar n [] acc = if n == 0 then (reverse acc,[]) else error "Wrong!"
+takePar n (x:xs) acc = if n == 0
+                       then (reverse acc, x:xs)
+                       else case x of
+                                    ')' -> takePar (n-1) xs (x:acc)
+                                    '(' -> takePar (n+1) xs (x:acc)
+                                    _ -> takePar n xs (x:acc)
+
+breakPar _ xs@[]           =  (xs, xs)
+breakPar p xs@(x:xs')
+           | x == '('   = let (a, b) = takePar 1 xs' []
+                              (y, z) = breakPar p b
+                          in ([x] ++ a ++ y, z)
+           | p x        =  ([],xs)
+           | otherwise  =  let (ys,zs) = breakPar p xs' in (x:ys,zs)
+
+main = do
+    let x = "1/2/3"
+    let y =  simplifyExpr $ simplifyExpr $ parseExpr x
+    print y
+    print $ eval y
