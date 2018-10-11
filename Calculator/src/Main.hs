4464ee73dbfa2279dde7dbf05171f9ce148d6608
Abbath <pitongogi@gmail.com>
2018-10-11 19:27:00 +0200

    Add semigroup

--- Calculator/src/Main.hs
+++ Calculator/src/Main.hs
@@ -4,6 +4,7 @@ module Main where
 import           Calculator
 import           Calculator.Tests
 import           Options.Applicative
+import           Data.Semigroup ((<>))
 
 data Options = Options {
       mp   :: Bool,
